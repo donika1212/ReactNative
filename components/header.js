ae79f3443bfdd1e7dce2d2077d0e4587b425b729
donika <donika.pllana@digitalschool.tech>
2026-04-20 18:05:43 +0200

    Module 11

--- components/header.js
+++ components/header.js
@@ -12,13 +12,14 @@ const Header = () =>{
 const styles = StyleSheet.create({
     container:{
         backgroundColor: "#F3E3D0",
-        padding: 15
+        padding: 20,
+        margin: 50,
     },
 
     title:{
         fontSize: 30,
         color: "#81A6C6",
-        textAlign: "center"
+        textAlign: "center",
     }
 });
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,29 +8,48 @@ import CustomButton from "../components/customButton";
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <View style={{ flex: 1 }}>
-      <Header />
+    <View style={styles.container}>
 
-      <CustomButton
-        title="Go to Profile"
-        onPress={() =>
-          navigation.navigate("Profile", {
-            name: "John Doe",
-            course: "Computer Science",
-          })
-        }
-      />
+  <Header />
 
-      <FlatList
-        data={students}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <StudentCard name={item.name} course={item.course} />
-        )}
-      />
-    </View>
+  <View style={styles.buttonRow}>
+    <CustomButton title="Add"/>
+    <CustomButton title="Delete"/>
+  </View>
+
+  <CustomButton
+    title="Go to Profile"
+    onPress={() =>
+      navigation.navigate("Profile", {
+        name: "John Doe",
+        course: "Computer Science",
+      })
+    }
+  />
+
+  <FlatList
+    data={students}
+    keyExtractor={(item) => item.id.toString()}
+    renderItem={({ item }) => (
+      <StudentCard name={item.name} course={item.course} />
+    )}
+  />
+</View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  buttonRow: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginBottom: 10,
+  },
+  
+})
 
 export default HomeScreen;
 
